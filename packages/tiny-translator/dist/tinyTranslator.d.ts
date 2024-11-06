9a9c697806b6cec8db95c0f85dea8c12e93ad438
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-11-06 18:24:54 +0900

    -

--- packages/tiny-translator/dist/tinyTranslator.d.ts
+++
@@ -1,4 +0,0 @@
-export default function tinyTranslator<L extends string>(translations: Record<string, Record<L, string>>): {
-    locale: (locale: L) => (key: keyof typeof translations, data?: any) => string;
-    get: (key: keyof typeof translations, data?: any) => string;
-};
