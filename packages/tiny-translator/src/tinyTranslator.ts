7a4296e1be873afa5312e863913ab37b8f9ddb5d
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-11-06 18:21:12 +0900

    -

--- packages/tiny-translator/src/tinyTranslator.ts
+++ packages/tiny-translator/src/tinyTranslator.ts
@@ -2,7 +2,12 @@ import Mustache from 'mustache';
 
 export default function tinyTranslator<L extends string>(
     translations: Record<string, Record<L, string>>
-) {
+): {
+    locale: (
+        locale: L
+    ) => (key: keyof typeof translations, data?: any) => string;
+    get: (key: keyof typeof translations, data?: any) => string;
+} {
     let _locale: L;
 
     const get = (key: keyof typeof translations, data?: any) => {
